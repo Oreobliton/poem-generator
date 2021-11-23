bff450660620c982822be0ae425c6e765d559b1b
Oreo <tristanpicard99@gmail.com>
2021-11-23 16:06:01 +0100

    First Commit

---
+++ src/markovian/Generation.py
@@ -0,0 +1,49 @@
+from MarkovWordTable import MarkovTable
+import random 
+from nonUninformChoice import *
+
+CORPUS_FILENAME = "Merge.txt"
+OUTPUT_FILENAME = "sentence.txt"
+NB_SENTENCES_TO_GENERATE = 300
+STARTING_WORD = "^"
+ENDING_WORD = "#"
+
+class MarkovianGenerator:
+
+    def __init__(self,markovTable):
+        self.markovTable = markovTable.getTransitionTable()
+            
+    def prochainMot(self, motCourrant):
+        #simuler un choix aléatoire à probabilité non uniformes
+        #Pour l'instant on se contente d'un choix uniforme
+        motsPossibles = self.markovTable[motCourrant]
+        return nonUniformChoice(motsPossibles)
+
+    def generation(self, n):
+        phrase = ""
+        #Remplacer le random start par un caractère de début/fin
+        motCourrant = STARTING_WORD
+        for i in range(n):
+            phrase += motCourrant + " "
+            try:
+                motCourrant = self.prochainMot(motCourrant)
+                if motCourrant == "":
+                    break
+            except KeyError:
+                break
+            if motCourrant == ENDING_WORD:
+                break
+        return phrase
+
+
+#TODO nettoyer cette boucle elle est plutôt sale
+m = MarkovTable(CORPUS_FILENAME)
+m.generateTable()
+m.dumpTable()
+gen = MarkovianGenerator(m)
+text = ""
+for i in range(NB_SENTENCES_TO_GENERATE):
+    text += gen.generation(500) + "\n"
+f = open(OUTPUT_FILENAME,"w")
+f.write(text)
+f.close()
---
+++ src/markovian/nonUninformChoice.py
@@ -0,0 +1,37 @@
+import random
+
+"""
+J'ai aucune idée de comment
+mon code fonctionne wtf.
+"""
+
+def frequency_list_builder(dico: dict()) -> tuple:
+    frequency_tuple_list = list()
+    current_frequency = 0
+    for key in dico.keys():
+        frequency_tuple_list.append((key, current_frequency + dico[key]))
+        current_frequency += dico[key]
+    return (frequency_tuple_list, current_frequency)
+
+def pick_elem_from_frequency_list(elemNumber: int, frequencylist: list) -> list:
+    """Renvois 
+    
+    """
+    if elemNumber < frequencylist[0][1]:
+        return frequencylist[0][0]
+    if elemNumber == frequencylist[-1][1]:
+        return frequencylist[-1][0]
+    for tuple in range(len(frequencylist)-1):
+        if (elemNumber >= frequencylist[tuple][1] and
+        elemNumber < frequencylist[tuple+1][1]):
+            return frequencylist[tuple+1][0]
+            
+def nonUniformChoice(dico: dict) -> str:
+    """Renvois un mot du dictionnaire 
+
+    Cette fonction selectionne un mot de manière aléatoire non-uniforme
+    dans un dictionnaire de couple (mot : occurences)
+    """
+    (frequency_tuple_list, cumulated_frequency) = frequency_list_builder(dico)
+    choice = random.randint(0, cumulated_frequency)
+    return pick_elem_from_frequency_list(choice, frequency_tuple_list)
